27839c0bd2c36441a05db56e74fb78ef251f70b8
sumeshkanayi <sumeshkanayi@gmail.com>
2017-12-18 13:07:26 +0530

    Update and rename seedJob.groovy to test.groovy

--- seedJob.groovy
+++
@@ -1,3 +0,0 @@
-import devops.seed.seedJob
-
-create_multibranch_job('blah/blah/com.rxcorp.test')
---
+++ test.groovy
@@ -0,0 +1,3 @@
+import devops.seed.seedJob
+
+seedJob.create_multibranch_job('blah/blah/com.rxcorp.test')
