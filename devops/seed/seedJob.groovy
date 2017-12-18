7ba46ec4473ca0d20405e6a6b56485061f4cd8b1
sumeshkanayi <sumeshkanayi@gmail.com>
2017-12-18 19:06:49 +0530

    Update seedJob.groovy

--- devops/seed/seedJob.groovy
+++ devops/seed/seedJob.groovy
@@ -77,5 +77,92 @@ public class seedJob {
             }
         }
     }
+    
+    
+    public static void create_pipeline_job(def dslFactory,def projectName) {
+
+        
+        def splitProjectNameToPathAndRepo = projectName.tokenize("/")
+        def repoName = splitProjectNameToPathAndRepo[-1]
+        splitProjectNameToPathAndRepo.pop()
+        def projectRoot = splitProjectNameToPathAndRepo.join("/")
+
+
+        String gitHost = 'ssh://git@git-sb.sumesh.com'
+
+
+
+        String FOLDER_CREDENTIALS_PROPERTY_NAME = 'com.cloudbees.hudson.plugins.folder.properties.FolderCredentialsProvider$FolderCredentialsProperty'
+
+        Node folderCredentialsPropertyNode
+        Item myFolder = Jenkins.instance.getItem(projectRoot)
+        if (myFolder) {
+            def folderCredentialsProperty = myFolder.getProperties().getDynamic(FOLDER_CREDENTIALS_PROPERTY_NAME)
+            if (folderCredentialsProperty) {
+                String xml = Items.XSTREAM2.toXML(folderCredentialsProperty)
+                folderCredentialsPropertyNode = new XmlParser().parseText(xml)
+            }
+        } else {
+
+
+            def projectRootArray = projectRoot.split("/")
+
+            String folderWalk = ""
+            projectRootArray.each {
+                folderWalk = folderWalk + "/" + it
+                def folderPresence = Jenkins.instance.getItem(folderWalk)
+                if (folderPresence == null) {
+                    dslFactory.folder(folderWalk) {
+
+
+                    }
+
+
+                }
+
+
+            }
+        }
+
+
+        dslFactory.folder(projectRoot) {
+            if (folderCredentialsPropertyNode) {
+                configure { project ->
+                    project / 'properties' << folderCredentialsPropertyNode
+                }
+                displayName projectRoot
+                description 'Build jobs for building ' + projectRoot + 'artifacts'
+
+            }
+        }
+
+
+        dslFactory.PipelineJob(projectRoot + '/' + repoName) {
+            branchSources {
+                git {
+                    remote(gitHost + '/' + projectRoot + '/' + repoName + '.git')
+                    credentialsId('gitlab_public_deploy_key')
+                }
+            }
+            orphanedItemStrategy {
+                discardOldItems {
+                    numToKeep(5)
+                }
+            }
+        }
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
