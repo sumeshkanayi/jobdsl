5e27d86685bfc0ba986907bdfd0c7ad241f8e8c2
sumeshkanayi <sumeshkanayi@gmail.com>
2017-12-18 19:29:58 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,6 +1,7 @@
 import devops.seed.seedJob
 
 seedJob.create_multibranch_job(this,'global/devops/us/india/com.rxcorp.test')
+seedJob.create_pipeline_job(this,'global/devops/us/india/com.rxcorp.testx')
 
 
 
