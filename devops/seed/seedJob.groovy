a4105fbed384e8761dd8597ec8ceaaaab174b0a6
sumeshkanayi <sumeshkanayi@gmail.com>
2017-12-18 12:17:13 +0530

    Create seedJob.groovy

---
+++ devops/seed/seedJob.groovy
@@ -0,0 +1,81 @@
+package devops.seed
+import jenkins.model.Jenkins
+import hudson.model.Item
+import hudson.model.Items
+
+class seedJob {
+
+    def create_multibranch_job(String projectName) {
+
+        projectName = "global/us/bdf/com.rxcorp.sample"
+        splitProjectNameToPathAndRepo = projectName.tokenize("/")
+        repoName = splitProjectNameToPathAndRepo[-1]
+        splitProjectNameToPathAndRepo.pop()
+        projectRoot = splitProjectNameToPathAndRepo.join("/")
+
+
+        String gitHost = 'ssh://git@git-sb.sumesh.com'
+
+
+
+        String FOLDER_CREDENTIALS_PROPERTY_NAME = 'com.cloudbees.hudson.plugins.folder.properties.FolderCredentialsProvider$FolderCredentialsProperty'
+
+        Node folderCredentialsPropertyNode
+        Item myFolder = Jenkins.instance.getItem(projectRoot)
+        if (myFolder) {
+            def folderCredentialsProperty = myFolder.getProperties().getDynamic(FOLDER_CREDENTIALS_PROPERTY_NAME)
+            if (folderCredentialsProperty) {
+                String xml = Items.XSTREAM2.toXML(folderCredentialsProperty)
+                folderCredentialsPropertyNode = new XmlParser().parseText(xml)
+            }
+        } else {
+
+
+            projectRootArray = projectRoot.split("/")
+
+            String folderWalk = ""
+            projectRootArray.each {
+                folderWalk = folderWalk + "/" + it
+                folderPresence = Jenkins.instance.getItem(folderWalk)
+                if (folderPresence == null) {
+                    folder(folderWalk) {
+
+
+                    }
+
+
+                }
+
+
+            }
+        }
+
+
+        folder(projectRoot) {
+            if (folderCredentialsPropertyNode) {
+                configure { project ->
+                    project / 'properties' << folderCredentialsPropertyNode
+                }
+                displayName projectRoot
+                description 'Build jobs for building ' + projectRoot + 'artifacts'
+
+            }
+        }
+
+
+        multibranchPipelineJob(projectRoot + '/' + repoName) {
+            branchSources {
+                git {
+                    remote(gitHost + '/' + projectRoot + '/' + repoName + '.git')
+                    credentialsId('gitlab_public_deploy_key')
+                }
+            }
+            orphanedItemStrategy {
+                discardOldItems {
+                    numToKeep(5)
+                }
+            }
+        }
+    }
+
+}
