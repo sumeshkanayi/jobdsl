04e245cbc8b4cc93cca1d706a56a2f948a53c702
sumeshkanayi <sumeshkanayi@gail.com>
2017-12-19 10:16:23 +0530

    tESTING

--- com/rxcorp/devops/jenkins/SeedJob.groovy
+++ com/rxcorp/devops/jenkins/SeedJob.groovy
@@ -91,7 +91,7 @@ class SeedJob {
         def projectRoot=projectMap["projectRoot"]
         def repoName=projectMap["repoName"]
         def gitHost = projectMap["gitHost"]
-        setup_jekins_folder(this,projectRoot)
+        setup_jekins_folder(dslFactory,projectRoot)
 
 
 
