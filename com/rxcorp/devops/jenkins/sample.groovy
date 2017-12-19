d448e22fa0b7f20617a59b7924025f17d4920532
sumeshkanayi <sumeshkanayi@gail.com>
2017-12-19 10:35:17 +0530

    testing dslfactory

---
+++ com/rxcorp/devops/jenkins/sample.groovy
@@ -0,0 +1,20 @@
+package com.rxcorp.devops.jenkins
+import jenkins.model.Jenkins
+import hudson.model.Item
+import hudson.model.Items
+
+/**
+ * Created by Sumesh on 19/12/17.
+ */
+class sample {
+
+    public static void createFolder(def dslFactory){
+
+        dslFactory.folder("Hello"){
+
+
+        }
+
+    }
+
+}
