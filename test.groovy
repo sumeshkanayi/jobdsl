0943baafc2e96f641a962d5f9f25beaba607a46b
sumeshkanayi <sumeshkanayi@gmail.com>
2017-12-19 10:49:33 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,11 +1,11 @@
-//import com.rxcorp.devops.jenkins.SeedJob
-import com.rxcorp.devops.jenkins.sample
+import com.rxcorp.devops.jenkins.SeedJob
+//import com.rxcorp.devops.jenkins.sample
 
-//SeedJob.create_multibranch(this,'global/devops/us/india/com.rxcorp.test')
+SeedJob.create_multibranch(this,'global/devops/us/india/com.rxcorp.test')
 
 //SeedJob.create_pipeline(this,'global/devops/us/india/com.rxcorp.testx')
 
-sample.createFolder(this,"poocha")
+//sample.createFolder(this,"poocha")
 
 
 
